49c18a87b588ba06321e6fec0d0c9e0d54079d69
Zoe Rehnberg <zrehnber@calpoly.edu>
2023-04-21 08:35:21 -0700

    update bonus murder

---
+++ challenges/bCH_murder_setup.R
@@ -0,0 +1,11 @@
+table_names <- c("crime_scene_report","drivers_license",
+                 "facebook_event_checkin",
+                 "get_fit_now_check_in","get_fit_now_member",
+                 "income", "interview","person")
+
+url_base <- "https://raw.githubusercontent.com/earobinson95/stat331-calpoly/master/practice-activities/data/sql-murder/"
+
+# For each table, read the table in and store it as the name of the table.
+purrr::walk(table_names, function(x) {
+  assign(x, readr::read_csv(paste0(url_base, x, ".csv")), envir = .GlobalEnv)
+})
